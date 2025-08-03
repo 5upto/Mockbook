ab900b70865c4dedf58b7aba256e3623022e1030
5upto <shawonghosh2002@gmail.com>
2025-08-04 00:50:19 +0530

    minor fix

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306
 };
 
 let connection = null;
